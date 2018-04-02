b5ab2fd6d334422314f1dc877aee313e19bf769c
KirillHaiduk <33031000+KirillHaiduk@users.noreply.github.com>
2018-04-02 22:12:02 +0300

    Update FilterFunction.cs

--- NET.S.2018.Haiduk.09/FilterExtended/FilterFunction.cs
+++ NET.S.2018.Haiduk.09/FilterExtended/FilterFunction.cs
@@ -1,4 +1,6 @@
-﻿namespace NET.S._2018.Haiduk._09
+using System;
+
+namespace NET.S._2018.Haiduk._09
 {
     public class FilterFunction : IPredicate
     {
@@ -6,14 +8,34 @@
 
         public FilterFunction(int filter)
         {
-            filterInteger = filter;
+            FilterInteger = filter;
         }
 
-        public bool Match(int digit)
+        public int FilterInteger
         {
-            if (filterInteger == digit)
+            get => filterInteger; set
             {
-                return true;
+                if (value < 0 || value > 9)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(filterInteger));
+                }
+
+                filterInteger = value;
+            }
+        }
+
+        public bool Match(int item)
+        {
+            int temp = item;
+            for (int i = 0; i < item.ToString().Length; i++)
+            {
+                var divisionResult = temp / 10;
+                int remainder = temp % 10;
+                temp = divisionResult;
+                if (Math.Abs(remainder) == FilterInteger)
+                {
+                    return true;
+                }
             }
 
             return false;
